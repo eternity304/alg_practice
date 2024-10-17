4a0a2b6a575ee1b8732640f42c5915b25c2cb0c8
eternity304 <harryzhou0304@gmail.com>
2024-10-17 11:33:30 -0400

    100,112

--- tree/sameTree.py
+++ tree/sameTree.py
@@ -22,3 +22,28 @@ def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
     
     check(p,q)
     return identical[0]
+
+def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+    identical = [True]
+
+    def check(p, q):
+        if p is None and q is None:
+            return True
+        elif (p is None and q is not None) or (p is not None and q is None):
+            identical[0] = False
+            return False
+        elif p.val != q.val:
+            identical[0] = False
+            return False
+        
+        if not check(p.left, q.right):
+            identical[0] = False
+            return False
+        elif not check(p.right, q.left):
+            identical[0] = False
+            return False
+        
+        return True
+    
+    check(root.left, root.right)
+    return identical[0]
